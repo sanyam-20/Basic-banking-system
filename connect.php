84c0f2bde41fe7c3b0018f1ae63a63adc36273e5
sanyam-20 <110491922+sanyam-20@users.noreply.github.com>
2023-03-14 11:33:07 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+    $db_connect = mysqli_connect("localhost","root","root123","banking");
+        if(!$db_connect){
+    die("Connection failed");
+}       
+?>
---
+++ customer.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customers</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<a href="./index.php">Home</a>
+<br><br>
+   <h1> All customers<br><br>
+Select to view the details</h1>
+   <table style="text-align:center; font-size: x-large;">
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+    </tr>
+    <?php
+    include 'connect.php';
+        $query = "SELECT ID,name from `customers`";
+        $conn = mysqli_query($db_connect,$query);
+        
+        while($row = mysqli_fetch_assoc($conn)){
+            echo"
+            <tr>
+            <td><a href='view.php?id={$row['ID']}'>".$row['ID']."</a></td>
+            <td><a href='view.php?id={$row['ID']}'>".$row['name']."</a></td>
+            </tr>";
+        }
+    ?>
+    </table>
+    <br>
+    <!-- <h1><a href="./view_transfer.php">View transactions</a></h1> -->
+    
+</body>
+</html>
